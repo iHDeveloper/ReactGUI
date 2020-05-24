a9c2c796ae75a9d0172b81ba5670df4552ff79be
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-05-24 14:27:42 +0300

    Implement download-build-tools task

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import de.undercouch.gradle.tasks.download.Download
+
 plugins {
     java
+    id ("de.undercouch.download") version "4.0.4"
 }
+
+val buildTools = BuildTools()
 
 group = "me.ihdeveloper"
 version = "0.1"
@@ -18,4 +23,32 @@ configure<JavaPluginConvention> {
 }
 
 tasks {
+
+    /**
+     *  Setup the workspace to develop the plugin
+     */
+    register("setup") {
+        dependsOn(":download-build-tools")
+    }
+
+    /**
+     * Download the build tools
+     */
+    register<Download>("download-build-tools") {
+        val temp = buildTools.temp
+        if (!temp.exists())
+            temp.mkdir()
+
+        if (temp.isFile)
+            error("Can't use the folder [.build-tools] because it's a file")
+
+        src("https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar")
+        dest(buildTools.file)
+    }
+
+}
+
+class BuildTools {
+    val temp = File(".build-tools")
+    val file = File(temp, "build-tools.jar")
 }
