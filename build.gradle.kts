fa29d2568ff16db868757b80adfc8a63ae066629
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-05-25 14:35:34 +0300

    Implement build common server task with printing EULA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import de.undercouch.gradle.tasks.download.Download
 
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+
 plugins {
     java
     id ("de.undercouch.download") version "4.0.4"
@@ -36,8 +39,11 @@ configure<JavaPluginConvention> {
 tasks {
 
     getByName("clean").doLast {
-        // Delete thr run server directory
+        // Delete the run server directory
         buildTools.runServerDir.deleteRecursively()
+
+        // Delete the common server directory
+        buildTools.commonServerDir.deleteRecursively()
     }
 
     /**
@@ -45,6 +51,7 @@ tasks {
      */
     register("setup") {
         dependsOn(":run-build-tools")
+        dependsOn(":build-common-server")
         dependsOn(":build-run-server")
     }
 
@@ -95,6 +102,62 @@ tasks {
     }
 
     /**
+     * Build the common server to be copied from later
+     */
+    register("build-common-server") {
+        onlyIf {
+            !buildTools.commonServerDir.exists()
+            !buildTools.commonServerPlugins.exists()
+            !buildTools.commonServerEULA.exists()
+        }
+
+        buildTools.commonServerDir.mkdir()
+        buildTools.commonServerPlugins.mkdir()
+
+        if (buildTools.commonServerEULA.exists()) {
+            var text = buildTools.commonServerEULA.readText()
+            text = text.replace("eula=false", "eula=true", true)
+            buildTools.commonServerEULA.writeText(text)
+        } else {
+            buildTools.commonServerEULA.writeText("eula=true")
+        }
+
+        // Print the EULA to the user
+        printEULA()
+
+        // Wait for 10 seconds to realise the message
+        try {
+            Thread.sleep(10 * 1000)
+        } catch (e: Exception) {}
+
+
+        copy {
+            from(buildTools.serverJar)
+            into(buildTools.commonServerDir)
+            rename {
+                "server.jar"
+            }
+        }
+
+        // Sends "stop" command to the common server to stop after initialising
+        val stopCommand = "stop"
+        val input = ByteArrayInputStream(stopCommand.toByteArray(StandardCharsets.UTF_8))
+
+        javaexec {
+            standardInput = input
+            workingDir = buildTools.commonServerDir
+            main = "-jar"
+            args = mutableListOf<String>(
+                    buildTools.commonServerJar.absolutePath
+            )
+        }
+
+        input.close()
+
+        buildTools.commonServerJar.delete()
+    }
+
+    /**
      * Build the run server for testing the plugin on it
      */
     register("build-run-server") {
@@ -134,6 +197,44 @@ tasks {
 
 }
 
+/**
+ * Print to the user that using the kit indicates that his/her agreement to Minecraft's EULA
+ */
+fun printEULA() {
+    val eulaInfo = mutableListOf<String>(
+            " _____________________________________________________________________________________",
+            "|  _________________________________________________________________________________  |",
+            "| |                                                                                 | |",
+            "| |                        ███████╗██╗   ██╗██╗      █████╗                         | |",
+            "| |                        ██╔════╝██║   ██║██║     ██╔══██╗                        | |",
+            "| |                        █████╗  ██║   ██║██║     ███████║                        | |",
+            "| |                        ██╔══╝  ██║   ██║██║     ██╔══██║                        | |",
+            "| |                        ███████╗╚██████╔╝███████╗██║  ██║                        | |",
+            "| |                                                                                 | |",
+            "| |                                                                                 | |",
+            "| |                [#] By using @iHDeveloper/spigot-starter-kit [#]                 | |",
+            "| |                                                                                 | |",
+            "| |              You are indicating your agreement to Minecraft's EULA              | |",
+            "| |               https://account.mojang.com/documents/minecraft_eula               | |",
+            "| |_________________________________________________________________________________| |",
+            "|_____________________________________________________________________________________|"
+    )
+
+    // Separate the EULA for more attention
+    for (i in 1..3) {
+        logger.lifecycle("")
+    }
+
+    for (i in eulaInfo) {
+        logger.lifecycle(i)
+    }
+
+    // Separate the EULA for more attention
+    for (i in 1..3) {
+        logger.lifecycle("")
+    }
+}
+
 class BuildTools (
         val minecraftVersion: String,
         val useSpigot: Boolean
@@ -142,6 +243,11 @@ class BuildTools (
     val file = File(buildDir, "build-tools.jar")
 
     val libsDir = File("build/libs/")
+
+    val commonServerDir = File("common_server")
+    val commonServerPlugins = File(commonServerDir, "plugins")
+    val commonServerJar = File(commonServerDir, "server.jar")
+    val commonServerEULA = File(commonServerDir, "eula.txt")
 
     val runServerDir = File("run_server")
     val runServerJar = File(runServerDir, "server.jar")
